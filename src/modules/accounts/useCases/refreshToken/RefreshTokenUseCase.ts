0cac0909e92577c15b0779ed3a0403b710d20dd2
YagoInacio <yagofaran@gmail.com>
2021-11-14 21:58:03 -0300

    created route to authenticate using refresh token

---
+++ src/modules/accounts/useCases/refreshToken/RefreshTokenUseCase.ts
@@ -0,0 +1,66 @@
+import { sign, verify } from 'jsonwebtoken';
+import { inject, injectable } from 'tsyringe';
+
+import { IUsersTokensRepository } from '@modules/accounts/repositories/IUsersTokensRepository';
+import { AppError } from '@shared/errors/AppError';
+
+interface IPayload {
+  sub: string;
+  email: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+  refreshToken: string;
+}
+
+@injectable()
+class RefreshTokenUseCase {
+  constructor(
+    @inject('UsersTokensRepository')
+    private usersTokensRepository: IUsersTokensRepository
+  ) {}
+
+  async execute(refreshToken: string): Promise<IResponse> {
+    const { sub: userId, email } = verify(
+      refreshToken,
+      process.env.SECRET_REFRESH_TOKEN
+    ) as IPayload;
+
+    const userToken =
+      await this.usersTokensRepository.findByUserIdAndRefreshToken(
+        userId,
+        refreshToken
+      );
+
+    if (!userToken) {
+      throw new AppError('Refresh token does not exist!');
+    }
+
+    if (userToken.expirationDate < new Date()) {
+      throw new AppError('Refresh token expired');
+    }
+
+    const token = sign({}, process.env.SECRET_TOKEN, {
+      subject: userId,
+      expiresIn: process.env.TOKEN_EXPIRES_IN,
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: userToken.user.name,
+        email,
+      },
+      refreshToken,
+    };
+
+    return tokenReturn;
+  }
+}
+
+export { RefreshTokenUseCase };
