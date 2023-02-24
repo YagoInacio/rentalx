08b5e5c8bc7553ca0fb54a168015c9dff23c7276
Yago Inacio <yagofaran@gmail.com>
2023-02-24 17:07:17 -0300

    created middleware to ensure that thee user is admin

--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,8 @@ import uploadConfig from '@config/upload';
 import { CreateCategoryController } from '@modules/cars/useCases/createCategory/createCategoryController';
 import { ImportCategoryController } from '@modules/cars/useCases/importCategory/ImportCategoryController';
 import { ListCategoriesController } from '@modules/cars/useCases/listCategories/ListCategoriesController';
+import { ensureAdmin } from '@shared/infra/http/middlewares/ensureAdmin';
+import { ensureAuthenticated } from '@shared/infra/http/middlewares/ensureAuthenticated';
 
 const categoriesRoutes = Router();
 
@@ -14,13 +16,20 @@ const createCategoryController = new CreateCategoryController();
 const listCategoriesController = new ListCategoriesController();
 const importCategoryController = new ImportCategoryController();
 
-categoriesRoutes.post('/', createCategoryController.handle);
+categoriesRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
 categoriesRoutes.get('/', listCategoriesController.handle);
 
 categoriesRoutes.post(
   '/import',
   uploadCategories.single('file'),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle
 );
 
